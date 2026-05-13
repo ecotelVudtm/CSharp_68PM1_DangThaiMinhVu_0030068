069e6fdff426fbf68f29a203cb73b192b7295d63
Đặng Thái Minh Vũ <vudtm@ecotel.com.vn>
2026-05-13 16:58:47 +0700

    Chức năng đăng nhập

---
+++ main.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace QL_SinhVIen
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_tenHeThong = new System.Windows.Forms.Label();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_tenHeThong
+            // 
+            this.lbl_tenHeThong.AutoSize = true;
+            this.lbl_tenHeThong.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lbl_tenHeThong.Location = new System.Drawing.Point(173, 19);
+            this.lbl_tenHeThong.Name = "lbl_tenHeThong";
+            this.lbl_tenHeThong.Size = new System.Drawing.Size(612, 58);
+            this.lbl_tenHeThong.TabIndex = 0;
+            this.lbl_tenHeThong.Text = "Hệ thống quản lý sinh viên";
+            this.lbl_tenHeThong.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Location = new System.Drawing.Point(385, 144);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(278, 22);
+            this.txt_Username.TabIndex = 1;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(385, 186);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(278, 22);
+            this.txt_Password.TabIndex = 1;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Location = new System.Drawing.Point(281, 147);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(98, 16);
+            this.lbl_Username.TabIndex = 2;
+            this.lbl_Username.Text = "Tên đăng nhập";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(281, 189);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(61, 16);
+            this.lbl_Password.TabIndex = 3;
+            this.lbl_Password.Text = "Mật khẩu";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(440, 246);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(110, 23);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 569);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_Username);
+            this.Controls.Add(this.lbl_tenHeThong);
+            this.Name = "main";
+            this.Text = "main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_tenHeThong;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_Login;
+    }
+}
